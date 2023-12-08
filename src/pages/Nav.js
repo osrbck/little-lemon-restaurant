ec8c74b302d094b065eb168c3d791a90de90f824
Osman S <osrbck@gmail.com>
2023-12-08 22:00:19 +0300

    meta tags created and added for now

--- src/pages/Nav.js
+++ src/pages/Nav.js
@@ -6,10 +6,10 @@ function Nav() {
     <nav>
       <img src={logo} alt="Little Lemon Logo" className="nav-logo" />
       <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/menu">Menu</a></li>
-        <li><a href="/reservations">Reservations</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="/" className='nav-link'>Home</a></li>
+        <li><a href="/menu" className='nav-link'>Menu</a></li>
+        <li><a href="/reservations" className='nav-link'>Reservations</a></li>
+        <li><a href="/contact" className='nav-link'>Contact</a></li>
       </ul>
     </nav>
   );
